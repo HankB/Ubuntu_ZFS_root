ff44c43e4b77892426b61b69b77f824c9051c580
Hank Barta <hbarta@gmail.com>
2018-12-11 12:53:55 -0600

    initial commit of initial setup

---
+++ env.sh
@@ -0,0 +1,9 @@
+# source this script to set these variables in the user environment
+# adjust to your needs
+set -u
+export DRIVE_ID=
+export ETHERNET=enp3s0
+export NEW_HOSTNAME=saugage
+export YOURUSERNAME=hbarta
+
+
